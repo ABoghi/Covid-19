ff1b18cd7403a10e1652da936de320cdaefd385b
Andrea Boghi <Andrea.BOGHI@nats.co.uk>
2020-04-04 15:52:59 +0100

    Added files.

---
+++ covid19model.py
@@ -0,0 +1,497 @@
+# data source: https://www.worldometers.info/coronavirus/country/uk/
+
+import numpy as np
+from matplotlib import pyplot as plt
+import datetime as dt
+
+# UK cases from '2020-02-27'
+N_uk = 65382556
+
+total_cases_per_day_uk = np.array([3, 4, 3, 13, 3, 12, 36, 29, 48, 45, 69, 43, 62,
+                                   77, 130, 208, 342, 251, 152, 407, 676, 643, 714,
+                                   1035, 665, 967, 1427, 1452, 2129, 2885, 2546,
+                                   2433, 2619, 3009, 4324, 4244, 4450])
+
+deaths_per_day_uk = np.array([0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 2, 1, 2, 2, 1,
+                              10, 14, 20, 16, 33, 40, 33, 56, 48, 54, 87, 43,
+                              115, 181, 260, 209, 180, 381, 563, 569, 684])
+
+recovery_per_day_uk = np.array([0, 0, 0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0,
+                                0, 0, 2, 32, 13, 0, 0, 0, 28, 0, 42, 0, 0, 0,
+                                0, 0, 0, 0, 0, 0, 0, 0])
+
+cases_per_day_uk = total_cases_per_day_uk - \
+    deaths_per_day_uk - recovery_per_day_uk
+
+# IT cases from '2020-02-20'
+N_it = 60480000
+
+total_cases_per_day_it = np.array([1, 17, 58, 78, 72, 94, 147, 185, 234, 239, 573, 335,
+                                   466, 587, 769, 778, 1247, 1492, 1797, 979, 2313,
+                                   2651, 2547, 3497, 3590, 3233, 3526, 4207, 5322,
+                                   5986, 6557, 5560, 4789, 5249, 5210, 6203, 5909,
+                                   5974, 5217, 4050, 4053, 4782, 4668, 4585])
+
+deaths_per_day_it = np.array([0, 0, 0, 0, 4, 4, 0, 5, 4, 8, 12, 11, 27, 28, 41,
+                              49, 36, 133, 97, 168, 196, 189, 250, 175, 368, 349,
+                              345, 475, 427, 627, 793, 651, 601, 743, 683, 712,
+                              919, 889, 756, 812, 837, 727, 760, 766])
+
+recovery_per_day_it = np.array([0, 1, 1, 0, -1, 1, 1, 42, 1, 4, 33, 66, 11, 116,
+                                138, 109, 66, 33, 102, 280, 41, 213, 181, 527,
+                                369, 414, 192, 1084, 415, 689, 943, 952, 408,
+                                894, 1036, 999, 589, 1434, 646, 1590, 1109,
+                                1118, 1431, 1480])
+
+cases_per_day_it = total_cases_per_day_it - \
+    deaths_per_day_it - recovery_per_day_it
+
+
+def first_differential(a: np.ndarray) -> np.ndarray:
+
+    n = len(a)
+    da = np.zeros(n)
+
+    da[0] = (-3.0 * a[0] + 4.0 * a[1] - a[2]) / 2.0
+    for k in range(1, n - 1):
+        da[k] = (a[k + 1] - a[k - 1]) / 2.0
+    da[n - 1] = (3.0 * a[n - 1] - 4.0 * a[n - 2] + a[n - 3]) / 2.0
+
+    return da
+
+
+def second_differential(a: np.ndarray) -> np.ndarray:
+
+    n = len(a)
+    d2a = np.zeros(n)
+
+    d2a[0] = 2.0 * a[0] - 5.0 * a[1] + 4.0 * a[2] - a[3]
+    for k in range(1, n - 1):
+        d2a[k] = a[k + 1] - 2.0 * a[k] + a[k + 1]
+    d2a[n - 1] = 2.0 * a[n-1] - 5.0 * a[n-2] + 4.0 * a[n-3] - a[n-4]
+
+    return d2a
+
+
+def integrate_per_day(data_per_day: np.ndarray) -> np.ndarray:
+
+    n = len(data_per_day)
+    data = np.zeros(n)
+    data[0] = data_per_day[0]
+    for i in range(1, n):
+        data[i] = data[i-1] + data_per_day[i]
+
+    return data
+
+
+def integrate_all(cases_per_day: np.ndarray = cases_per_day_uk,
+                  deaths_per_day: np.ndarray = deaths_per_day_uk,
+                  recovery_per_day: np.ndarray = recovery_per_day_uk) -> np.ndarray:
+
+    total_cases = integrate_per_day(cases_per_day)
+    total_deaths = integrate_per_day(deaths_per_day)
+    total_recovery = integrate_per_day(recovery_per_day)
+
+    return total_cases, total_deaths, total_recovery
+
+
+def prepare_SIR_model(cases_per_day: np.ndarray = cases_per_day_uk,
+                      deaths_per_day: np.ndarray = deaths_per_day_uk,
+                      recovery_per_day: np.ndarray = recovery_per_day_uk,
+                      total_population: int = N_uk, vital_dynamics: bool = True) -> np.ndarray:
+
+    total_cases, total_deaths, total_recovery = integrate_all(
+        cases_per_day, deaths_per_day, recovery_per_day)
+
+    # is the cumulative?
+    I = total_cases / total_population
+    D = total_deaths / total_population
+    R = D + total_recovery / total_population
+    # is the dayly rate?
+    I = cases_per_day / total_population
+    D = deaths_per_day / total_population
+    R = D + recovery_per_day / total_population
+    S = 1.0 - I - R
+
+    dI = first_differential(I)
+    dR = first_differential(R)
+    dS = - dI - dR
+
+    if vital_dynamics:
+        gamma_sc, lambda_sc, mu_sc = parameters_with_vital_dynamics(
+            S, I, R, dS, dI, dR)
+    else:
+        gamma_sc, lambda_sc, mu_sc = parameters_without_vital_dynamics(
+            S, I, R, dS, dI, dR)
+
+    return I, R, S, gamma_sc, lambda_sc, mu_sc
+
+
+def parameters_with_vital_dynamics(S: np.ndarray, I: np.ndarray, R: np.ndarray,
+                                   dS: np.ndarray, dI: np.ndarray, dR: np.ndarray):
+
+    n = len(I)
+
+    lambda_field = np.zeros(n)
+    gamma_field = np.zeros(n)
+    mu_field = np.zeros(n)
+    x_old = np.array([1, 1, 1])
+    for k in range(n):
+        b = np.array([dS[k], dI[k], dR[k]])
+        A = np.array([[- S[k] * I[k], 0.0, I[k] + R[k]],
+                      [S[k] * I[k], -I[k], -I[k]], [0.0, I[k], - R[k]]])
+        A1 = np.matmul(A.T, A)
+        b1 = np.matmul(A.T, b)
+        try:
+            x = np.linalg.solve(A1, b1)
+            x_old = x
+        except:
+            x = x_old
+        lambda_field[k] = x[0]
+        gamma_field[k] = x[1]
+        mu_field[k] = x[2]
+
+    lambda_sc = abs(np.mean(np.ma.masked_invalid(lambda_field)))
+    gamma_sc = abs(np.mean(np.ma.masked_invalid(gamma_field)))
+    mu_sc = abs(np.mean(np.ma.masked_invalid(mu_field)))
+
+    print(f"Preconditioned lambda = {lambda_sc}[1/day]")
+    print(f"Preconditioned gamma = {gamma_sc}[1/day]")
+    print(f"Preconditioned mu = {mu_sc}[1/day]")
+
+    E = 0.0
+    E = SIR_functional(S, I, R, dS, dI, dR, lambda_sc, gamma_sc, mu_sc, n)
+    print(f"Initial Error = {E}[1/day]")
+
+    b = np.array([0.0, 0.0, 0.0])
+    A = np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]])
+
+    b0 = 0.0
+    for k in range(n):
+        b0 += -(dS[k] - dI[k]) * S[k] * I[k]
+
+    b[0] = b0/n
+
+    b1 = 0.0
+    for k in range(n):
+        b1 += -(dI[k] - dR[k]) * I[k]
+
+    b[1] = b1/n
+
+    b2 = 0.0
+    for k in range(n):
+        b2 += -(dS[k]*(S[k] - 1.0) + dI[k] * I[k] + dR[k] * R[k])
+
+    b[2] = b2/n
+
+    A00 = 0.0
+    for k in range(n):
+        A00 += 2.0 * S[k] * S[k] * I[k] * I[k]
+
+    A[0][0] = A00/n
+
+    A01 = 0.0
+    for k in range(n):
+        A01 += - S[k] * I[k] * I[k]
+
+    A[0][1] = A01/n
+    A[1][0] = A01/n
+
+    A02 = 0.0
+    for k in range(n):
+        A02 += (S[k] - 1.0 - I[k]) * S[k] * I[k]
+
+    A[0][2] = A02/n
+    A[2][0] = A02/n
+
+    A11 = 0.0
+    for k in range(n):
+        A11 += 2.0 * I[k] * I[k]
+
+    A12 = 0.0
+    for k in range(n):
+        A12 += (I[k] - R[k]) * I[k]
+
+    A[1][2] = A12/n
+    A[2][1] = A12/n
+
+    A22 = 0.0
+    for k in range(n):
+        A22 += ((S[k] - 1.0) * (S[k] - 1.0) + I[k] * I[k] + R[k] * R[k])
+
+    A[2][2] = A22/n
+
+    x = np.linalg.solve(A, b)
+    if x[0] > 0.0:
+        lambda_sc = x[0]
+        print(f"Best Fit lambda = {lambda_sc}[1/day]")
+    else:
+        print(f"Negative lambda, use preconditioned value.")
+
+    if x[1] > 0.0:
+        gamma_sc = x[1]
+        print(f"Best Fit gamma = {gamma_sc}[1/day]")
+    else:
+        print(f"Negative gamma, use preconditioned value.")
+
+    if x[2] > 0.0:
+        mu_sc = x[2]
+        print(f"Best Fit mu = {mu_sc}[1/day]")
+    else:
+        print(f"Negative mu, use preconditioned value.")
+
+    E = SIR_functional(S, I, R, dS, dI, dR, lambda_sc, gamma_sc, mu_sc, n)
+    print(f"Final Error = {E}[1/day]")
+
+    '''                   
+    dEdlambda = 0.0
+    for k in range(n):
+        dEdlambda += (dS[k] - dI[k]) * S[k] * I[k] \
+            + 2.0 * lambda_sc * S[k] * S[k] * I[k] * I[k] \
+                - gamma_sc * I[k] * S[k] * I[k] \
+                    + mu_sc * (S[k] - 1.0 - I[k]) * S[k] * I[k]
+         
+    dEdgamma = 0.0
+    for k in range(n):
+        dEdgamma += (dI[k] - dR[k]) * I[k] \
+            - lambda_sc * S[k] * I[k] * I[k] \
+                + 2.0 * gamma_sc * I[k] * I[k] \
+                    + mu_sc * (I[k] - R[k]) * I[k]
+                
+    dEdmu = 0.0
+    for k in range(n):
+        dEdmu += (dS[k]*(S[k] - 1.0) + dI[k] * I[k] + dR[k]* R[k]) \
+            + lambda_sc * (S[k] - 1.0 - I[k]) * S[k] * I[k] \
+                + gamma_sc * I[k] * (I[k] - R[k]) \
+                    + mu_sc * ((S[k] - 1.0) * (S[k] - 1.0) + I[k] * I[k] + R[k] * R[k])
+    '''
+
+    return gamma_sc, lambda_sc, mu_sc
+
+
+def parameters_without_vital_dynamics(S: np.ndarray, I: np.ndarray, R: np.ndarray,
+                                      dS: np.ndarray, dI: np.ndarray, dR: np.ndarray):
+
+    mu_sc = 0.0
+
+    gamma_field = (dR + mu_sc * R) / I
+    gamma_sc = np.mean(gamma_field)
+
+    lambda_field = (dI + (gamma_sc + mu_sc) * I) / (S * I)
+    lambda_sc = np.mean(lambda_field)
+
+    print(f"Preconditioned lambda = {lambda_sc}[1/day]")
+    print(f"Preconditioned gamma = {gamma_sc}[1/day]")
+
+    n = len(I)
+
+    E = SIR_functional(S, I, R, dS, dI, dR, lambda_sc, gamma_sc, mu_sc, n)
+    print(f"Initial Error = {E}[1/day]")
+
+    b = np.array([0.0, 0.0])
+    A = np.array([[0.0, 0.0], [0.0, 0.0]])
+
+    b0 = 0.0
+    for k in range(n):
+        b0 += -(dS[k] - dI[k]) * S[k] * I[k]
+
+    b[0] = b0/n
+
+    b1 = 0.0
+    for k in range(n):
+        b1 += -(dI[k] - dR[k]) * I[k]
+
+    b[1] = b1/n
+
+    A00 = 0.0
+    for k in range(n):
+        A00 += 2.0 * S[k] * S[k] * I[k] * I[k]
+
+    A[0][0] = A00/n
+
+    A01 = 0.0
+    for k in range(n):
+        A01 += - S[k] * I[k] * I[k]
+
+    A[0][1] = A01/n
+    A[1][0] = A01/n
+
+    A11 = 0.0
+    for k in range(n):
+        A11 += 2.0 * I[k] * I[k]
+
+    A[1][1] = A11/n
+
+    x = np.linalg.solve(A, b)
+    if x[0] > 0.0:
+        lambda_sc = x[0]
+        print(f"Best Fit lambda = {lambda_sc}[1/day]")
+    else:
+        print(f"Negative lambda, use preconditioned value.")
+
+    if x[1] > 0.0:
+        gamma_sc = x[1]
+        print(f"Best Fit gamma = {gamma_sc}[1/day]")
+    else:
+        print(f"Negative gamma, use preconditioned value.")
+
+    E = SIR_functional(S, I, R, dS, dI, dR, lambda_sc, gamma_sc, mu_sc, n)
+    print(f"Final Error = {E}[1/day]")
+
+    '''                
+    dEdlambda = 0.0
+    for k in range(n):
+        dEdlambda += (dS[k] - dI[k]) * S[k] * I[k] \
+            + 2.0 * lambda_sc * S[k] * S[k] * I[k] * I[k] \
+                - gamma_sc * I[k] * S[k] * I[k]
+         
+    dEdgamma = 0.0
+    for k in range(n):
+        dEdgamma += (dI[k] - dR[k]) * I[k] \
+            - lambda_sc * S[k] * I[k] * I[k] \
+                + 2.0 * gamma_sc * I[k] * I[k]
+    '''
+
+    return gamma_sc, lambda_sc, mu_sc
+
+
+def SIR_functional(S: np.ndarray, I: np.ndarray, R: np.ndarray,
+                   dS: np.ndarray, dI: np.ndarray, dR: np.ndarray,
+                   lambda_sc: float, gamma_sc: float, mu_sc: float, n: int):
+
+    E = 0.0
+    for k in range(n):
+        E += 0.5 * (dS[k] + lambda_sc * S[k] * I[k] - mu_sc + mu_sc * S[k])**2.0 + \
+            0.5 * (dI[k] - lambda_sc * S[k] * I[k] + (gamma_sc + mu_sc) * I[k])**2.0 + \
+            0.5 * (dR[k] - gamma_sc * I[k] + mu_sc * R[k])**2.0
+
+    return E
+
+
+def SIR_model(I: float, R: float, gamma_sc: float, lambda_sc: float, mu_sc: float) -> float:
+
+    S = 1.0 - I - R
+    dIdt = lambda_sc * S * I - (gamma_sc + mu_sc) * I
+    dRdt = gamma_sc * I - mu_sc * R
+    # at equilibrium:
+    # S_eq = (gamma_sc + mu_sc)/lambda_sc
+    # I_eq = mu_sc * (lambda_sc - gamma_sc - mu_sc)/(lambda_sc * (gamma_sc + mu_sc) )
+    # R_eq = gamma_sc * (lambda_sc - gamma_sc - mu_sc)/(lambda_sc * (gamma_sc + mu_sc) )
+
+    return S, dIdt, dRdt
+
+
+def runge_kutta_4_SIR(I_old: float, R_old: float, time_step: float, gamma_sc: float, lambda_sc: float, mu_sc: float) -> float:
+
+    # step 1
+    S_old, dIdt_1, dRdt_1 = SIR_model(I_old, R_old, gamma_sc, lambda_sc, mu_sc)
+    I_temp = I_old + (time_step/2.0) * dIdt_1
+    R_temp = R_old + (time_step/2.0) * dRdt_1
+
+    # step 2
+    S_temp, dIdt_2, dRdt_2 = SIR_model(
+        I_temp, R_temp, gamma_sc, lambda_sc, mu_sc)
+    I_temp = I_old + (time_step/2.0) * dIdt_2
+    R_temp = R_old + (time_step/2.0) * dRdt_2
+
+    # step 3
+    S_temp, dIdt_3, dRdt_3 = SIR_model(
+        I_temp, R_temp, gamma_sc, lambda_sc, mu_sc)
+    I_temp = I_old + time_step * dIdt_3
+    R_temp = R_old + time_step * dRdt_3
+
+    # step 4
+    S_temp, dIdt_4, dRdt_4 = SIR_model(
+        I_temp, R_temp, gamma_sc, lambda_sc, mu_sc)
+    I_new = I_old + (time_step / 6.0) * (dIdt_1 + 2.0 *
+                                         dIdt_2 + 2.0 * dIdt_3 + dIdt_4)
+    R_new = R_old + (time_step / 6.0) * (dRdt_1 + 2.0 *
+                                         dRdt_2 + 2.0 * dRdt_3 + dRdt_4)
+    S_new = 1.0 - I_new - R_new
+
+    return I_new, R_new, S_new
+
+
+def run_SIR_model(step_number: int, time_step: int = 1.0,
+                  country: str = 'uk',
+                  offset: int = 0,
+                  vital_dynamics: bool = True) -> np.ndarray:
+
+    if country == 'uk':
+        cases_per_day = cases_per_day_uk[offset:]
+        deaths_per_day = deaths_per_day_uk[offset:]
+        recovery_per_day = recovery_per_day_uk[offset:]
+        total_population = N_uk
+    elif country == 'it':
+        cases_per_day = cases_per_day_it[offset:]
+        deaths_per_day = deaths_per_day_it[offset:]
+        recovery_per_day = recovery_per_day_it[offset:]
+        total_population = N_it
+    else:
+        print(f"Data for this country not stored. Abort")
+        return
+
+    Id, Rd, Sd, gamma_sc, lambda_sc, mu_sc = prepare_SIR_model(
+        cases_per_day, deaths_per_day, recovery_per_day, total_population,
+        vital_dynamics)
+
+    I = np.zeros(step_number)
+    R = np.zeros(step_number)
+    S = np.zeros(step_number)
+
+    I[0] = Id[0]
+    R[0] = Rd[0]
+    S[0] = Sd[0]
+    for k in range(1, step_number):
+
+        I[k], R[k], S[k] = runge_kutta_4_SIR(
+            I[k-1], R[k-1], time_step, gamma_sc, lambda_sc, mu_sc)
+
+    total_cases, total_deaths, total_recovery = integrate_all(
+        cases_per_day, deaths_per_day, recovery_per_day)
+
+    # , total_cases, (total_deaths + total_recovery)
+    return I * total_population, R * total_population, cases_per_day, (deaths_per_day + recovery_per_day)
+
+
+def plot_results(Im: np.ndarray, Rm: np.ndarray, Id: np.ndarray, Rd: np.ndarray, total_population: int = N_uk):
+
+    nm = len(Im)
+    nd = len(Id)
+
+    xm = np.linspace(0, nm-1, nm)
+    xd = np.linspace(0, nd-1, nd)
+
+    fig = plt.figure()
+    plt.semilogy(xm, Im, 'r', label='model')
+    plt.semilogy(xd, Id, 'o', label='data')
+    plt.semilogy(xm, total_population * np.ones(nm),
+                 '--k', label='total population')
+    plt.legend(loc='best')
+    # plt.xlabel('days since beginning')
+    plt.ylabel('Total Infected')
+    plt.title('SIR model with Vital Dynamics (UK)')
+    plt.grid()
+    # UK
+    plt.xticks([0, 26, 53, 80], ["27/02/2020",
+                                 "25/03/2020", "21/04/2020", "18/05/2020"])
+    # UK removing the first 8
+    # plt.xticks([0, 18, 45, 72], ["05/03/2020", "25/03/2020", "21/04/2020" , "18/05/2020"])
+    fig.savefig(f'fig1.png')
+
+    fig = plt.figure()
+    plt.semilogy(xm, Rm, 'r', label='model')
+    plt.semilogy(xd, Rd, 'o', label='data')
+    plt.semilogy(xm, total_population * np.ones(nm),
+                 '--k', label='total population')
+    plt.legend(loc='best')
+    # plt.xlabel('days since beginning')
+    plt.ylabel('Total Outcomes')
+    plt.title('SIR model with Vital Dynamics (UK)')
+    plt.grid()
+    # UK
+    plt.xticks([0, 26, 53, 80], ["27/02/2020",
+                                 "25/03/2020", "21/04/2020", "18/05/2020"])
+    fig.savefig(f'fig2.png')
+
+    return
